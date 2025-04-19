20f236ebe90a1ccc95c09b028cc88e25fc7bd352
shinedog <15728019+shinedog@users.noreply.github.com>
2025-04-19 12:56:20 +0000

    added basic job and inputs from nixpkgs-unstable for hello world

--- flake.nix
+++ flake.nix
@@ -1,4 +1,20 @@
 {
   description = "A Simple Flake for Testing a Hydra Installation and Jobset Configuration";
-  outputs = { self }: { };
+    inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { 
+    self,
+    nixpkgs
+     }: {
+    
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+    hydraJobs = {
+      inherit (self)
+        defaultPackage;
+    };
+    };
 }
