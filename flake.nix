e76911015d2325f86fc839d1d2473ae05078d31b
Shinedog <15728019+shinedog@users.noreply.github.com>
2025-04-20 21:36:16 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,6 +12,15 @@
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
 
+    hydraSpecs =
+        let
+          nfj = b: hydralib.flakeJob "github:shinedog/laughing-potato/${b}";
+        in {
+          jobsets = hydralib.makeSpec {
+            nixcfg-main        = nfj "main";
+            nixcfg-auto-update = nfj "auto-update";
+          };
+        };
     hydraJobs = {
       inherit (self)
         defaultPackage;
