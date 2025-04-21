5aa0384c8a2102796967743a44fa1a5349faa0da
Shinedog <15728019+shinedog@users.noreply.github.com>
2025-04-20 21:38:20 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -12,6 +12,34 @@
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
 
+rec {
+  jobsetDefaults = {
+    enabled = 1;
+    hidden = false;
+    keepnr = 3;
+    schedulingshares = 100;
+    checkinterval = 15;
+    emailoverride = "";
+    type = 1;
+  };
+
+  flakeJob = flakeRef: jobsetDefaults // {
+    flake = flakeRef;
+  };
+
+  makeSpec = contents: builtins.derivation {
+    name = "spec.json";
+    system = "x86_64-linux";
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+    builder = "/bin/sh";
+    args = [ (builtins.toFile "builder.sh" ''
+      echo "$contents" > $out
+    '') ];
+    contents = builtins.toJSON contents;
+  };
+}
+
     hydraSpecs =
         let
           nfj = b: hydralib.flakeJob "github:shinedog/laughing-potato/${b}";
