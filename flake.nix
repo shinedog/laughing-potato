9d2147205ebd066975377898ea0d346a27d46879
Shinedog <15728019+shinedog@users.noreply.github.com>
2025-05-19 18:32:29 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,13 @@
 {
-  description = "Hydra fallback flake";
+  description = "Safe static Hydra flake";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-  outputs = { nixpkgs, ... }: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      hydraJobs = {
-        mergeCheck = pkgs.runCommand "merge-check" {} ''
-          echo ok > $out
-        '';
-      };
+  outputs = { nixpkgs, ... }: {
+    hydraJobs = {
+      mergeCheck = nixpkgs.legacyPackages.x86_64-linux.runCommand "merge-check" {} ''
+        echo "Static merge check passed." > $out
+      '';
     };
+  };
 }
