cf2f360e5301492bec9e4b1b871804d8a6ba2181
Shinedog <15728019+shinedog@users.noreply.github.com>
2025-06-06 12:59:09 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,15 +1,7 @@
 {
-  description = "Minimal test flake for Hydra";
-  
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-  
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    
-    hydraJobs = {
-      hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  outputs = { nixpkgs, ... }: {
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.hello;
+    hydraJobs.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
   };
 }
